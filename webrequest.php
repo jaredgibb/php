2381316ef9e6ec453a8080545067a94ce1fcfd7a
jaredgibb <70493366+jaredgibb@users.noreply.github.com>
2022-01-06 14:49:31 -0500

    Update webrequest.php

--- webrequest.php
+++ webrequest.php
@@ -1,1 +1,19 @@
-echo "Hello, World!";
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://cataas.com/cat/says/hello%2520world!',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
